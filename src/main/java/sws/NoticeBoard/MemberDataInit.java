4a64cbeeaba8966f61463cbe771e8994035f44af
bw02184 <bw02184@naver.com>
2023-07-23 19:14:24 +0900

    GitHub Action 추가

--- src/main/java/sws/NoticeBoard/MemberDataInit.java
+++ src/main/java/sws/NoticeBoard/MemberDataInit.java
@@ -3,6 +3,7 @@ package sws.NoticeBoard;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import sws.NoticeBoard.domain.Member;
@@ -10,13 +11,14 @@ import sws.NoticeBoard.repository.MemberRepository;
 
 @Component
 @RequiredArgsConstructor
-public class DeleteMemberDataInit {
+public class MemberDataInit {
   private final InitService initService;
 
   @PostConstruct
   public void init() {
     initService.dbInit1();
   }
+
   /** 탈퇴회원 전용 데이터 추가 */
   @Component
   @Transactional
@@ -24,6 +26,7 @@ public class DeleteMemberDataInit {
   static class InitService {
     private final EntityManager em;
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void dbInit1() {
       Member findMember = memberRepository.findByLoginId("deleted");
@@ -37,6 +40,31 @@ public class DeleteMemberDataInit {
       member.setEmail("delete@delete.sws");
       member.setIsAdmin(false);
       em.persist(member);
+
+      Member findMember1 = memberRepository.findByLoginId("test");
+      if (findMember1 != null) {
+        return;
+      }
+      Member member1 = new Member();
+      member1.setRealName("sws");
+      member1.setLoginId("test");
+      member1.setPassword(
+          passwordEncoder.encode("test")); // 일부러 passwordEncoder 미사용(로그인 되지 않도록 하기 위해)
+      member1.setEmail("sws@sws.sws");
+      member1.setIsAdmin(false);
+      em.persist(member1);
+
+      Member findMember2 = memberRepository.findByLoginId("test");
+      if (findMember2 != null) {
+        return;
+      }
+      Member member2 = new Member();
+      member2.setRealName("sws2");
+      member2.setLoginId("test2");
+      member2.setPassword(passwordEncoder.encode("test2"));
+      member2.setEmail("sws2@sws.sws");
+      member2.setIsAdmin(false);
+      em.persist(member2);
     }
   }
 }
